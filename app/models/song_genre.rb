acfcafb4a71550f66961430979c82dfb6d4a9165
Veniel Dorestave <veniel@noukod.com>
2019-01-17 20:58:33 +0000

    Automatically backed up by Learn

--- app/models/song_genre.rb
+++ app/models/song_genre.rb
@@ -1,4 +1,11 @@
-class SongGenre < ActiveRecord::Base
-  belongs_to :song
-  belongs_to :genre
+# class SongGenre < ActiveRecord::Base
+#   belongs_to :song
+#   belongs_to :genre
+# end
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
